f39b886359e3928e7ff2ab2851a9f586d8528d91
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-23 11:35:03 +0100

    added cosmos db functionality

--- Main-Service/Main/__init__.py
+++ Main-Service/Main/__init__.py
@@ -1,7 +1,19 @@
 import logging
 import requests
 import azure.functions as func
+from azure.cosmos import exceptions, CosmosClient, PartitionKey, partition_key
 
+endpoint = 'https://user-gen-db.documents.azure.com:443/'
+key = ''
+
+client = CosmosClient(endpoint, key)
+
+database = client.create_database_if_not_exists(id="user-gen-rand-names")
+container = database.create_container_if_not_exists(
+    id = 'user-gen-rand-names-container',
+    partition_key= PartitionKey(path="/username"),
+    offer_throughput=400
+)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -15,6 +27,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     while count < 5:
         newstr += letters[count] + numbers[count]
         count += 1
+
+    container.create_item(body={
+        "id": str(1),
+        "username": newstr
+        })
 
     return func.HttpResponse(
         newstr,
